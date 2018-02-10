3085192b86bd3f73f71ff71e09528adb5486f298
Wellington <wellington30071997@gmail.com>
2018-02-10 12:56:00 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# React Ninja
+
+Esse é um mini-projeto para o estudo relâmpago de ReactJs com webpack.
+
+### Pré-requisito
+
+Instalação global:
+
+  - `node` e `npm`
+  - `webpack` e `webpack-dev-server`
+
+### Começo rapido
+
+```sh
+$ npm install
+$ webpack
+$ webpack-dev-server
+```
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+	entry: path.join(__dirname, 'src', 'index'),
+	
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+
+	module: {
+		loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+			loader: 'babel',
+			query: {
+				presets:['react']
+			}
+    }]
+	},
+	resolveLoader: {
+    moduleExtensions: ['-loader']
+	}
+};
