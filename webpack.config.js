83f66e8f58cf607e8984656155470e8117528ddf
Wellington <wellington30071997@gmail.com>
2018-02-10 13:39:04 -0200

    Node server implemented and react-hot-loader configuration initialization

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-	entry: path.join(__dirname, 'src', 'index'),
+	devtool: 'source-map',
+	entry: [
+		'react-hot-loader/patch',
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
+		path.join(__dirname, 'src', 'index')
+	],
 	
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'bundle.js',
 		publicPath: '/static/'
 	},
+
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
 
 	module: {
 		loaders: [{
@@ -23,6 +34,6 @@ module.exports = {
     }]
 	},
 	resolveLoader: {
-    moduleExtensions: ['-loader']
+	    moduleExtensions: ['-loader']
 	}
 };
