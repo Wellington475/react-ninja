48813c09c6eea8470232f716a351675e1e12c7d9
Wellington <wellington30071997@gmail.com>
2018-02-10 14:09:17 -0200

    Implemented react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-  <div>Hello world!</div>,
-  document.querySelector('#app')
-);
+import App from './containers/App';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.querySelector('#app'),
+  );
+};
+
+render(App);
+
+if (module.hot)
+  module.hot.accept('./containers/App', () => render(App));
